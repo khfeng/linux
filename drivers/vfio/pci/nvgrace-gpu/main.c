0799457fde9b5317837cbd46ca89b966d029e89d
Matthew R. Ochs <mochs@nvidia.com>
2024-11-07 15:55:58 -0800

    vfio/nvgrace-gpu: Handle EGM registration failure
    Detect and handle a failure from the EGM registration service.
    
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>
    Acked-by: Kai-Heng Feng <kaihengf@nvidia.com>
    Acked-by: Carol L. Soto <csoto@nvidia.com>
    Acked-by: Koba Ko <kobak@nvidia.com>
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>

--- drivers/vfio/pci/nvgrace-gpu/main.c
+++ drivers/vfio/pci/nvgrace-gpu/main.c
@@ -1076,7 +1076,10 @@ static int nvgrace_gpu_probe(struct pci_dev *pdev,
 		}
 
 		if (egm_enabled) {
-			register_egm_node(pdev);
+			ret = register_egm_node(pdev);
+			if (ret)
+				goto out_put_vdev;
+
 			nvdev->egm_node = egmpxm;
 		}
 
@@ -1084,7 +1087,7 @@ static int nvgrace_gpu_probe(struct pci_dev *pdev,
 
 	ret = vfio_pci_core_register_device(&nvdev->core_device);
 	if (ret)
-		goto out_put_vdev;
+		goto out_egm_unreg;
 
 #ifdef CONFIG_MEMORY_FAILURE
 	/*
@@ -1096,6 +1099,9 @@ static int nvgrace_gpu_probe(struct pci_dev *pdev,
 
 	return ret;
 
+out_egm_unreg:
+	if (egm_enabled)
+		unregister_egm_node(nvdev->egm_node);
 out_put_vdev:
 	vfio_put_device(&nvdev->core_device.vdev);
 	return ret;
